2a7ca036e08cea6140a2e56a8361ce2c84e08618
AlexMato <matontialessandro0@gmail.com>
2020-03-27 14:45:06 +0100

    prima stampa dei dati

---
+++ main.js
@@ -0,0 +1,24 @@
+//inserire nome
+var nomeUtente = document.getElementById('nome');
+//inserire km da percorrere
+var kmDaPercorrere = document.getElementById('km');
+//inserire età
+var etaUtente = document.getElementById('eta');
+
+//collegare il bottone calcolo
+var bottoneCalcolo =document.getElementById('calcolo');
+
+bottoneCalcolo.addEventListener("click",
+  function(){
+    var valoreUtente = nomeUtente.value;
+    var valoreKm = kmDaPercorrere.value;
+    var valoreEta = etaUtente.value;
+    var prova = valoreUtente + valoreKm + valoreEta;
+
+    console.log(prova);
+  }
+
+)
+//stampare prezzo in base ai dati inseriti
+
+//annullare dati inseriti
