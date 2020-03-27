5efd867d2b134a7bf646c563764a0ed274093393
AlexMato <matontialessandro0@gmail.com>
2020-03-27 15:05:49 +0100

    stampa del nome nel biglietto

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+
+
 //inserire nome
 var nomeUtente = document.getElementById('nome');
 //inserire km da percorrere
@@ -8,17 +10,23 @@ var etaUtente = document.getElementById('eta');
 //collegare il bottone calcolo
 var bottoneCalcolo =document.getElementById('calcolo');
 
+
+
 bottoneCalcolo.addEventListener("click",
   function(){
     var valoreUtente = nomeUtente.value;
     var valoreKm = kmDaPercorrere.value;
     var valoreEta = etaUtente.value;
     var prova = valoreUtente + valoreKm + valoreEta;
-
     console.log(prova);
+    document.getElementById('passeggero').innerHTML = valoreUtente;
+
   }
 
 )
+
+
+
 //stampare prezzo in base ai dati inseriti
 
 //annullare dati inseriti
